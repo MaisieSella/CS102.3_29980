0d388106f215c1436437e5f35fc3825bc98cb7fe
IPMSand <131543563+IPMSand@users.noreply.github.com>
2023-07-29 16:26:42 +0530

    Add files via upload

---
+++ lab_6/sala.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int x=2;
+while(x<=1000)
+{ printf(" %d", x);
+x=x+2;  //x+=2;
+
+}
+	}
+
+
+
---
+++ lab_6/vtest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+  int no1,no2,add,sub,mult,div,mod;
+  printf("Enter first number ");
+  scanf("%d",&no1);
+  printf("Enter sec number ");
+  scanf("%d",&no2);
+  add=no1+no2;
+  sub=no1-no2;
+  mult=no1*no2;
+  div=no1/no2;
+  mod=no1%no2;
+  printf("Addition is %d\n",add);
+
+}
