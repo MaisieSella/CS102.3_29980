37a560a0b9b83afa36ae4c2d63048fdf79385e27
IPMSand <131543563+IPMSand@users.noreply.github.com>
2023-07-29 15:13:57 +0530

    Add files via upload

--- lab_1/main.c
+++ lab_1/main.c
@@ -1,12 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
+    int num;
+    char name[15];
+    float numdec1;
+    double numdec2;
 
-printf("*\n");
-printf("**\n");
-printf("***\n");
-printf("****\n");
-printf("*****\n");
+    printf("Enter integer: ");
+    scanf("%d",&num);
 
-return 0;
+     printf("Enter your name: ");
+    scanf("%s",&name);
+
+    printf("Enter decimal number: ");
+    scanf("%f",&numdec1);
+
+    printf("Enter another decimal number: ");
+    scanf("%lf",&numdec2);
+
+    printf("%d \n",num);
+    printf("%.2f \n",numdec1);
+    printf("%.2f \n",numdec2);
+    printf("%s\n",name);
+
+
+	return 0;
 }
