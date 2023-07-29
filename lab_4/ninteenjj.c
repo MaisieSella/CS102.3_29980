1b1575478e7275c6891508ac515962471dbc63c2
IPMSand <131543563+IPMSand@users.noreply.github.com>
2023-07-29 16:04:19 +0530

    Add files via upload

---
+++ lab_4/ninteenjj.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int month;
+printf("Enter a month number:");
+scanf("%d",&month);
+switch(month)
+ if (month>=1 && month<=12)
+
+{
+
+    case 1:case 3:case 5:case 7:case 9: case 8: case 10: case 12:printf(":31 days ");break;
+    case 4:case 6: case 11:printf(":30 days");break;
+    case 2:printf(":28 days");break;
+
+
+
+
+}
+}
+
