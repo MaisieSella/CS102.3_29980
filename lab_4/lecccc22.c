bd29dc52875d63252df51f2c79a50a2006ba738f
IPMSand <131543563+IPMSand@users.noreply.github.com>
2023-07-29 16:04:59 +0530

    Add files via upload

---
+++ lab_4/lecccc22.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num,oddcount=0,evencount=0,count=1;
+    while(count<10)
+    {
+        printf("Enter number %d:",count++);
+        scanf("%d",&num);
+        if(num==-99)
+        {
+            break;}
+          if(num%2==0)
+            evencount++;
+            else
+            oddcount++;
+        }
+
+        printf("even %d\n odd %d \n",evencount,oddcount);
+}
