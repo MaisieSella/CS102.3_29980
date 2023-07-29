2e3a6d6ab777141aa41431ac3edbe046c30fba8c
IPMSand <131543563+IPMSand@users.noreply.github.com>
2023-07-29 15:41:53 +0530

    Add files via upload

---
+++ lab_1/main8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	printf("The color: %s\n", "blue");//The color:blue
+	printf("First number: %d\n", 12345);//1234
+	printf("Second number: %04d\n", 25);//0025
+	printf("Third number: %i\n", 1234);//1234
+	printf("Float number: %3.2f\n", 3.14159);//3.14
+	printf("Hexadecimal: %x\n", 255);//ff
+	printf("Octal: %o\n", 255);//377
+	printf("Unsigned value: %u\n", 150);//15
+	printf("Just print the percentage sign %%\n", 10);//%
+}
+
