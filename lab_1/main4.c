37599d5a375f01118b403fad89b242d5feeb76db
IPMSand <131543563+IPMSand@users.noreply.github.com>
2023-07-29 15:40:07 +0530

    Add files via upload

---
+++ lab_1/main4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+   {
+	int x, y, sum;
+    printf("Input the first integer: \n");
+    scanf("%d", &x);
+    printf("Input the second integer: \n");
+    scanf("%d", &y);
+    sum = x + y;
+    printf("Sum of the above two integers = %d\n", sum);
+    return 0;
+}
+
+
