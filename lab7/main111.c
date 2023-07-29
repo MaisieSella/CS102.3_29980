75e1986f39f1dfb9b99f55c61012f8791f933ae3
IPMSand <131543563+IPMSand@users.noreply.github.com>
2023-07-29 16:35:19 +0530

    Add files via upload

---
+++ lab7/main111.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void n1()
+{
+    int num1,num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&num1,&num2);
+    //calculate and display
+    printf("sum: %d\n",num1+num2);
+    printf("difference :%d\n",num1-num2);
+
+}int main()
+{
+    //calling the above function
+    n1();
+}
+//tutorial 7-1st question
