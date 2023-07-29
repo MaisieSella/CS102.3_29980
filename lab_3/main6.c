a45693daedb2a69f38c3b0eca2e32c7328614a61
IPMSand <131543563+IPMSand@users.noreply.github.com>
2023-07-29 15:51:13 +0530

    Add files via upload

---
+++ lab_3/main6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+ printf("Integer equivalents of some uppercase letters:\n");
+ printf("A: %d\n", 'A');
+ printf("B: %d\n", 'B');
+ printf("C: %d\n", 'C');
+ printf("Integer equivalents of some lowercase letters:\n");
+ printf("a: %d\n", 'a');
+ printf("b: %d\n", 'b');
+ printf("c: %d\n", 'c');
+ printf("Integer equivalents of some digits:\n");
+ printf("0: %d\n", '0');
+ printf("1: %d\n", '1');
+ printf("2: %d\n", '2');
+ printf("Integer equivalents of some special symbols:\n");
+ printf("$: %d\n", '$');
+ printf("*: %d\n", '*');
+ printf("+: %d\n", '+');
+ printf("/: %d\n", '/');
+ printf("Blank character: %d\n", ' ');
+ return 0;
+}
